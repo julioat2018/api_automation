6a23730dfbedc2f6f65e545e33cf7e3f58bde0c2
julioat2018 <julioat2018@gmail.com>
2020-06-01 16:50:21 -0300

    adding selenium automation

--- api/views.py
+++ api/views.py
@@ -15,10 +15,25 @@ def auto_submit(request):
     surname = request.POST.get('surname')
     password = request.POST.get('password')
     email = request.POST.get('email')
+    phone = request.POST.get('phone')
     site_id = request.POST.get('site_id')
+
+    url = 'https://energia.tariffe-speciali.it/fv_optima_settembre2018/'
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    br = webdriver.Chrome(options=options, executable_path=settings.DIR_PATH + 'browser/chromedriver')
+    br.set_page_load_timeout(40)
+
+    # options = webdriver.FirefoxOptions()
+    # options.add_argument('--headless')
+    # browser = webdriver.Firefox(options=options, executable_path=settings.DIR_PATH + 'browser/geckodriver')
 
     if site_id == 'energia':
         print("energia")
+        br.find_element_by_xpath('//*[@id="nome"]').send_keys(name)
+        br.find_element_by_xpath('//*[@id="cognome"]').send_keys(surname)
+        br.find_element_by_xpath('//*[@id="telefono"]').send_keys(phone)
+        br.find_element_by_xpath('//*[@id="form-cliente"]/div/div[5]/div/input').click()
         pass
     elif site_id == 'telefonia':
         print("telefonia")
