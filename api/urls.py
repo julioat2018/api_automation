91bd4c3c1fba76b39667787e834bbc293e8bb354
julioat2018 <julioat2018@gmail.com>
2020-06-02 19:57:28 -0300

    adding logic

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('auto_submit/', views.auto_submit, name='auto_submit'),
+    path('auto_submit', views.auto_submit, name='auto_submit'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.conf import settings
+
 from selenium import webdriver
 
 import time
@@ -12,45 +14,51 @@ def index(request):
 
 @require_http_methods(["POST"])
 def auto_submit(request):
-    name = request.POST.get('name')
-    surname = request.POST.get('surname')
-    password = request.POST.get('password')
-    email = request.POST.get('email')
-    phone = request.POST.get('phone')
-    site_id = request.POST.get('site_id')
+    name = request.POST.get('name', '')
+    surname = request.POST.get('surname', '')
+    password = request.POST.get('password', '')
+    email = request.POST.get('email', '')
+    phone = request.POST.get('phone', '')
+    site_id = request.POST.get('site_id', '')
 
     options = webdriver.ChromeOptions()
     # options.add_argument('--headless')
-    br = webdriver.Chrome(options=options, executable_path=settings.DIR_PATH + 'browser/chromedriver')
+    br = webdriver.Chrome(options=options, executable_path=settings.BASE_DIR + settings.DIR_PATH + 'chromedriver')
     br.set_page_load_timeout(40)
 
     # options = webdriver.FirefoxOptions()
     # options.add_argument('--headless')
-    # browser = webdriver.Firefox(options=options, executable_path=settings.DIR_PATH + 'browser/geckodriver')
+    # br = webdriver.Chrome(options=options, executable_path=settings.BASE_DIR + settings.DIR_PATH + 'geckodriver')
 
-    if site_id == 'energia':
-        print("energia")
-        url = 'https://energia.tariffe-speciali.it/fv_optima_settembre2018/'
-        br.get(url)
-        time.sleep(1)
+    try:
+        if site_id == 'energia':
+            url = 'https://energia.tariffe-speciali.it/fv_optima_settembre2018/'
+            br.get(url)
+            time.sleep(1)
 
-        br.find_element_by_xpath('//*[@id="nome"]').send_keys(name)
-        br.find_element_by_xpath('//*[@id="cognome"]').send_keys(surname)
-        br.find_element_by_xpath('//*[@id="telefono"]').send_keys(phone)
-        br.find_element_by_xpath('//*[@id="form-cliente"]/div/div[5]/div/input').click()
-        pass
-    elif site_id == 'telefonia':
-        print("telefonia")
-        url = 'https://telefonia.tariffe-speciali.it/diamond_fibra/conferma.php'
-        br.get(url)
-        time.sleep(1)
+            br.find_element_by_xpath('//*[@id="nome"]').send_keys(name)
+            br.find_element_by_xpath('//*[@id="cognome"]').send_keys(surname)
+            br.find_element_by_xpath('//*[@id="telefono"]').send_keys(phone)
+            br.find_element_by_xpath('//*[@id="form-cliente"]/div/div[5]/div/input').click()
+            time.sleep(10)
+        elif site_id == 'telefonia':
+            url = 'https://telefonia.tariffe-speciali.it/diamond_fibra/conferma.php'
+            br.get(url)
+            time.sleep(1)
 
-        br.find_element_by_xpath('//*[@id="nome"]').send_keys(name)
-        br.find_element_by_xpath('//*[@id="cognome"]').send_keys(surname)
-        br.find_element_by_xpath('//*[@id="telefono"]').send_keys(phone)
-        br.find_element_by_xpath('//*[@id="form-cliente"]/div/div[5]/div/input').click()
-        pass
+            br.find_element_by_xpath('//*[@id="nome"]').send_keys(name)
+            br.find_element_by_xpath('//*[@id="cognome"]').send_keys(surname)
+            br.find_element_by_xpath('//*[@id="telefono"]').send_keys(phone)
+            # br.find_element_by_xpath('//*[@id="form-cliente"]/div/div[5]/div/input').click()
+            pass
+        else:
+            return JsonResponse({'result': 'Bad request'})
+    except Exception as e:
+        print(e)
+        br.close()
+        return JsonResponse({'result': 'Fail'})
 
-    return HttpResponse("Hello, world. You're at the api auto_submit.")
+    br.close()
+    return JsonResponse({'result': 'Success'})
 
 
