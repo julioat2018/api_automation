3b54bb50d4d99d6994776cb9d1673e4840f8082b
julioat2018 <julioat2018@gmail.com>
2020-06-05 10:21:52 -0300

    adding socks5 proxy

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 import re
 
 # from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 from seleniumwire import webdriver
 
 import time
@@ -35,7 +36,7 @@ def auto_submit(request):
         # options.add_argument("--proxy-auto-detect")
         br = webdriver.Chrome(options=options, executable_path=settings.BASE_DIR + settings.DIR_PATH + 'chromedriver')
         br.maximize_window()
-        br.set_page_load_timeout(40)
+        br.set_page_load_timeout(100)
 
         options = webdriver.FirefoxOptions()
         options.add_argument('--headless')
@@ -62,32 +63,33 @@ def auto_submit(request):
             if site_id == 'energia':
                 url = 'https://energia.tariffe-speciali.it/fv_optima_settembre2018/'
                 br.get(url)
-                time.sleep(1)
 
                 br.find_element_by_xpath('//*[@id="nome"]').send_keys(name)
                 br.find_element_by_xpath('//*[@id="cognome"]').send_keys(surname)
                 br.find_element_by_xpath('//*[@id="telefono"]').send_keys(phone)
+                time.sleep(5)
                 # br.find_element_by_xpath('//*[@id="form-cliente"]/div/div[5]/div/input').click()
                 br.find_element_by_xpath('//input[@type="submit"]').click()
                 br.close()
-                time.sleep(10)
             elif site_id == 'telefonia':
                 url = 'https://telefonia.tariffe-speciali.it/diamond_fibra/conferma.php'
                 br.get(url)
-                time.sleep(1)
 
                 br.find_element_by_xpath('//*[@id="nome"]').send_keys(name)
                 br.find_element_by_xpath('//*[@id="cognome"]').send_keys(surname)
                 br.find_element_by_xpath('//*[@id="telefono"]').send_keys(phone)
                 # br.find_element_by_xpath('//*[@id="form-cliente"]/div/div[5]/div/input').click()
                 br.close()
-                time.sleep(10)
             else:
                 return JsonResponse({'result': 'Bad request'})
-        except Exception as e:
+        except TimeoutException as e:
             print(e)
             br.close()
-            return JsonResponse({'result': 'Fail'})
+            return JsonResponse({'result': 'TimeoutException'})
+        except ElementClickInterceptedException as e:
+            print(e)
+            br.close()
+            return JsonResponse({'result': 'ElementClickInterceptedException'})
 
         break
 
